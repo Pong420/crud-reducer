fe9255fd9be117469f2f4302ec8620d167c1073a
Pong <samfunghp@gmail.com>
2020-08-31 23:22:30 +0800

    feat: add selector

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './bindDispatch';
 export * from './crudActions';
 export * from './crudReducer';
+export * from './selector';
 export * from './useActions';
 export * from './useCRUDReducer';
