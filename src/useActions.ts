2f3a77cd20c90315abaad585fe37146fb062e362
Pong <samfunghp@gmail.com>
2020-10-18 16:33:25 +0800

    fix: use actions

--- src/useActions.ts
+++ src/useActions.ts
@@ -1,20 +1,12 @@
-import { useRef, useMemo } from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { ActionCreators } from './crudAction';
 import { Dispatched, bindDispatch } from './bindDispatch';
-
-export type ActionsMap<T1 extends { type: string }> = {
-  [K in T1['type']]: T1 extends { type: K } ? T1 : never;
-};
-
-export type UnionActions<
-  T extends Record<string, (...args: any[]) => any>
-> = ReturnType<T[keyof T]>;
 
 export function useActions<A extends ActionCreators>(
   creators: A
 ): Dispatched<A> {
   const dispatch = useDispatch();
-  const creatorsRef = useRef(creators);
-  return useMemo(() => bindDispatch(creatorsRef.current, dispatch), [dispatch]);
+  const [actions] = useState(bindDispatch(creators, dispatch));
+  return actions;
 }
