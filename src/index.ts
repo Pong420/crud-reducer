38b696df49656788f2ed5950ba804a4d6bf9ba0c
Pong <samfunghp@gmail.com>
2021-08-18 17:55:03 +0800

    remove old source code

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './bindDispatch';
-export * from './crudAction';
-export * from './crudReducer';
-export * from './crudSelector';
-export * from './useActions';
-export * from './useCRUDReducer';
